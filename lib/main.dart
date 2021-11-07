f9464720cfda62376edd541c84eb8dee3c5f468d
Michael Mogenson <michael.mogenson@gmail.com>
2021-11-07 11:16:09 -0500

    update to null safety

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,43 @@ import 'dart:convert';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _currentColor = Colors.pink;
+  final _controller = CircleColorPickerController(
+    initialColor: Colors.pink,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wiz Color',
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: _currentColor,
           title: Text('Wiz Color'),
           centerTitle: true,
         ),
         body: Center(
           child: CircleColorPicker(
-            initialColor: Colors.blue,
+            controller: _controller,
             onChanged: (color) {
-              final command = '{"method":"setPilot","params":{"r":${color.red},"g":${color.green},"b":${color.blue}}}';
+              setState(() => _currentColor = color);
+            },
+            onEnded: (color) {
+              final command =
+                  '{"method":"setPilot","params":{"r":${color.red},"g":${color.green},"b":${color.blue}}}';
               print(command);
-              RawDatagramSocket.bind(InternetAddress.anyIPv4, 38899).then((socket) {
+              RawDatagramSocket.bind(InternetAddress.anyIPv4, 38899)
+                  .then((socket) {
                 socket.broadcastEnabled = true;
-                socket.send(utf8.encode(command), InternetAddress('255.255.255.255'), 38899);
+                socket.send(utf8.encode(command),
+                    InternetAddress('255.255.255.255'), 38899);
+                socket.close();
               });
             },
           ),
